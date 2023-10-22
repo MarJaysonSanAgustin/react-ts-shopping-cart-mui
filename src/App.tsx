8e6be13a33472b9210fcab2ed29ddb0313b423f1
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2023-10-22 15:11:45 +0800

    chore(setup): remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <Button variant="contained">Hello</Button>
+      </div>
+    </Box>
+  );
 }
 
 export default App;
