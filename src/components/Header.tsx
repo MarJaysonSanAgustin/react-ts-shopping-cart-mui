8f43d30aa849258a9a47af0a3d50ca58d4f3d37b
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2023-10-23 16:14:45 +0800

    feat: add shopping cart toggle functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
-import * as React from "react";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import VillaIcon from "@mui/icons-material/Villa";
+
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 interface HeaderProps {
   title: string;
@@ -10,24 +15,29 @@ interface HeaderProps {
 
 function Header(props: HeaderProps) {
   const { title } = props;
+  const { openCart, cartQuantity } = useShoppingCart();
 
   return (
-    <React.Fragment>
+    <AppBar position="sticky" sx={{ backgroundColor: "white" }} elevation={0}>
       <Toolbar sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <VillaIcon color="primary" fontSize="large" />
         <Typography
           component="h2"
           variant="h5"
           color="inherit"
+          align="center"
           noWrap
           sx={{ flex: 1 }}
         >
           {title}
         </Typography>
-        <IconButton>
-          <ShoppingCartIcon />
+        <IconButton onClick={openCart}>
+          <Badge badgeContent={cartQuantity} color="primary">
+            <ShoppingCartIcon />
+          </Badge>
         </IconButton>
       </Toolbar>
-    </React.Fragment>
+    </AppBar>
   );
 }
 
