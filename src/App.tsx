19db7a258896a5696a24653e0ad9da1eb0292772
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2023-10-23 16:14:26 +0800

    feat: add shopping cart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 import Header from "./components/Header";
+import StoreItem from "./components/StoreItem";
+import storeItems from "./data/items.json";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <Header title="The Ultra Rich Shopping" />
-      <main></main>
-    </Container>
+    <ShoppingCartProvider>
+      <Container maxWidth="lg">
+        <Header title="The Ultra Rich Shopping" />
+        <main style={{ paddingTop: 20, marginBottom: 30 }}>
+          <Grid container spacing={2}>
+            {storeItems.map((item) => (
+              <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
+                <StoreItem {...item} />
+              </Grid>
+            ))}
+          </Grid>
+        </main>
+      </Container>
+    </ShoppingCartProvider>
   );
 }
 
